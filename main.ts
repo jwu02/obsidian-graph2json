24ab99dbc077bee7558676636887cb2a5e8c9795
jwu02 <jiacongwu31@gmail.com>
2025-03-13 22:11:02 +0000

    One shotted plugin with Cursor...

--- main.ts
+++ main.ts
@@ -1,134 +1,114 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, TFile } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface GraphNode {
+    id: string;
+    group: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface GraphEdge {
+    from: string;
+    to: string;
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+interface GraphData {
+    nodes: GraphNode[];
+    edges: GraphEdge[];
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+export default class GraphToJsonPlugin extends Plugin {
+    async onload() {
+        // Add a ribbon icon
+        this.addRibbonIcon('graph', 'Export Graph to JSON', () => {
+            this.exportGraphToJson();
+        });
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+        // Add a command
+        this.addCommand({
+            id: 'export-graph-to-json',
+            name: 'Export Graph to JSON',
+            callback: () => {
+                this.exportGraphToJson();
+            }
+        });
+    }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+    async exportGraphToJson() {
+        const app = this.app;
+        const activeLeaf = app.workspace.activeLeaf;
+        console.log('Active leaf:', activeLeaf);
+        
+        if (!activeLeaf) {
+            console.error('No active leaf found');
+            return;
+        }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+        const viewState = activeLeaf.getViewState();
+        console.log('View state:', viewState);
+        
+        if (viewState.type !== 'graph') {
+            console.error('No graph view is active. Please open the Graph view first (View -> Graph).');
+            console.error('Current view type:', viewState.type);
+            return;
+        }
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+        // Get all markdown files from CardsPublic folder
+        const allFiles = app.vault.getMarkdownFiles();
+        const files = allFiles.filter(file => file.path.startsWith('CardsPublic/'));
+        console.log('Processing files from CardsPublic folder:', files.length);
+        
+        const nodes: GraphNode[] = [];
+        const edges: GraphEdge[] = [];
 
-	display(): void {
-		const {containerEl} = this;
+        // Create nodes
+        for (const file of files) {
+            const relativePath = app.vault.getResourcePath(file);
+            const directory = relativePath.split('/').slice(0, -1).join('/');
+            const id = file.basename;
 
-		containerEl.empty();
+            nodes.push({
+                id,
+                group: directory
+            });
+        }
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+        // Get links between files
+        for (const file of files) {
+            const content = await app.vault.read(file);
+            const links = content.match(/\[\[(.*?)\]\]/g) || [];
+
+            for (const link of links) {
+                const targetPath = link.slice(2, -2);
+                const targetFile = app.metadataCache.getFirstLinkpathDest(targetPath, file.path);
+
+                if (targetFile && targetFile.extension === 'md' && targetFile.path.startsWith('CardsPublic/')) {
+                    edges.push({
+                        from: file.basename,
+                        to: targetFile.basename
+                    });
+                }
+            }
+        }
+
+        // Create the final graph data
+        const graphData: GraphData = {
+            nodes,
+            edges
+        };
+
+        // Convert to JSON and save
+        const jsonContent = JSON.stringify(graphData, null, 2);
+        const jsonPath = 'graph_data.json';
+
+        try {
+            const existingFile = app.vault.getAbstractFileByPath(jsonPath);
+            if (existingFile) {
+                await app.vault.modify(existingFile as TFile, jsonContent);
+            } else {
+                await app.vault.create(jsonPath, jsonContent);
+            }
+            console.log('Graph data exported successfully to graph.json');
+        } catch (error) {
+            console.error('Error exporting graph data:', error);
+        }
+    }
 }
